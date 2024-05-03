c6ea3843e83ba7fda48ef4b1d4c48903c4b4c760
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-03 19:26:04 +0100

    Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,7 +46,15 @@ export default function Footer(){
                               </div>
                         </div>
                 </div>
-                <p className="texte-footer">Copyright@2022 Allright reserved - art-Charlot</p>
+                <p className="texte-footer mb-3">Copyright@2022 Art Charlot Allright reserved</p>
+                <div className="mb-3">
+                  Created by
+                  <a
+                      href="https://charlot-dedjinou.vercel.app"
+                      className="fw-bold text-white text-decoration-none"
+                  >
+                  Charlot DEDJINOU</a>
+                  </div>
          </footer>
     )
 }
